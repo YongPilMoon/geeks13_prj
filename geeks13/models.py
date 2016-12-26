7c5e5808be37eccf013f7fae3884f370c2fc9391
YongPilMoon <m71661@gmail.com>
2016-12-26 13:14:04 +0900

    Add User,Post models

---
+++ geeks13/models.py
@@ -0,0 +1,27 @@
+from geeks13 import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    content = db.Column(db.String(120))
+
+    def __init__(self, title=None, content=None):
+        self.title = title
+        self.content = content
+
+    def __repr__(self):
+        return '<Title %r>' % (self.title)
