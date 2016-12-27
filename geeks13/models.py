d1382a355dac0d6adb04f0ee6b4b8f0f58ce67d5
YongPilMoon <m71661@gmail.com>
2016-12-27 15:41:36 +0900

    비밀번호 필드와 해시화하는 함수 추가

--- geeks13/models.py
+++ geeks13/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from geeks13 import db
 
 
@@ -5,10 +7,17 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    pw_hash = db.Column(db.String(50))
 
     def __init__(self, username, email):
         self.username = username
         self.email = email
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
 
     def __repr__(self):
         return '<User %r>' % (self.username)
